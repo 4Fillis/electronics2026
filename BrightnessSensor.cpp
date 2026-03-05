8ff3a19e01361877944efc83dfc80de9d2c6a696
illiotes <165102269+4Fillis@users.noreply.github.com>
2026-03-06 10:43:41 +1300

    Changed brightness sensor pin name to BRIGHTNESSSENSOR amd removed the print of the square ratio

--- BrightnessSensor.cpp
+++ BrightnessSensor.cpp
@@ -11,6 +11,5 @@ float reading = analogRead(LIGHTSENSORPIN); //Read light level
 float square_ratio = reading / 1023.0; //Get percent of maximum value (1023)
 square_ratio = pow(square_ratio, 2.0);
 Serial.println(reading);
-Serial.println(square_ratio);
 delay(300);
 }
