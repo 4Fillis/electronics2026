f5dc6e547982c3e45596028cab410994ec965678
illiotes <165102269+4Fillis@users.noreply.github.com>
2026-03-09 14:55:28 +1300

    Changed BMP380_reading to arduino project hub code & re-did explanatory comments

--- BMP280_reading.cpp
+++ BMP280_reading.cpp
@@ -1,72 +1,57 @@
-/*********
-  Complete project details at https://randomnerdtutorials.com  
-*********/
+/* This code is to use with Adafruit BMP280
+ * It measures temperature, pressure, altitude and displays them on the Serial monitor
+ * Modified version of the Adafruit example code
+ Credit: https://projecthub.arduino.cc/SurtrTech/bmp280-measure-temperature-pressure-and-altitude-6002cd
+*/
 
-#include <Wire.h>
-#include <Adafruit_Sensor.h>
-#include <Adafruit_BME280.h>
+#include
+  <Adafruit_BMP280.h>
 
-/*#include <SPI.h>
-#define BME_SCK 18
-#define BME_MISO 19
-#define BME_MOSI 23
-#define BME_CS 5*/
+Adafruit_BMP280 bmp; // I2C Interface
 
-#define SEALEVELPRESSURE_HPA (1013.25)
-
-Adafruit_BME280 bme; // I2C
-//Adafruit_BME280 bme(BME_CS); // hardware SPI
-//Adafruit_BME280 bme(BME_CS, BME_MOSI, BME_MISO, BME_SCK); // software SPI
-
-unsigned long delayTime;
-
-void setup() {
+void setup()
+  {
   Serial.begin(9600);
-  Serial.println(F("BME280 test"));
+  Serial.println(F("BMP280 test"));
 
-  bool status;
-
-  // default settings
-  // (you can also pass in a Wire library object like &Wire2)
-  status = bme.begin(0x76);  
-  if (!status) {
-    Serial.println("Could not find a valid BME280 sensor, check wiring!");
+  if
+  (!bmp.begin()) {
+    Serial.println(F("Could not find a valid BMP280 sensor,
+  check wiring!"));
     while (1);
   }
 
-  Serial.println("-- Default Test --");
-  delayTime = 1000;
+  /* Default settings from datasheet.
+  */
+  bmp.setSampling(Adafruit_BMP280::MODE_NORMAL,     /* Operating Mode. */
 
-  Serial.println();
+                  Adafruit_BMP280::SAMPLING_X2,     /* Temp. oversampling */
+
+                  Adafruit_BMP280::SAMPLING_X16,    /* Pressure oversampling */
+
+                  Adafruit_BMP280::FILTER_X16,      /* Filtering. */
+                  Adafruit_BMP280::STANDBY_MS_500);
+  /* Standby time. */
 }
 
+void loop() {
+    Serial.print(F("Temperature
+  = "));
+    Serial.print(bmp.readTemperature());
+    Serial.println(" *C");
 
-void loop() { 
-  printValues();
-  delay(delayTime);
-}
 
-void printValues() {
-  Serial.print("Temperature = ");
-  Serial.print(bme.readTemperature());
-  Serial.println(" *C");
-  
-  // Convert temperature to Fahrenheit
-  /*Serial.print("Temperature = ");
-  Serial.print(1.8 * bme.readTemperature() + 32);
-  Serial.println(" *F");*/
-  
-  Serial.print("Pressure = ");
-  Serial.print(bme.readPressure() / 100.0F);
-  Serial.println(" hPa");
+    Serial.print(F("Pressure = "));
+    Serial.print(bmp.readPressure()/100);
+  //displaying the Pressure in hPa, you can change the unit
+    Serial.println("
+  hPa");
 
-  Serial.print("Approx. Altitude = ");
-  Serial.print(bme.readAltitude(SEALEVELPRESSURE_HPA));
-  Serial.println(" m");
+    Serial.print(F("Approx altitude = "));
+    Serial.print(bmp.readAltitude(1019.66));
+  //"1019.66" is the standard pressure(hPa) at sea level (regional)
+    Serial.println("m");  //If unknown: modify until current altitude is given
 
-  Serial.print("Humidity = ");
-  Serial.print(bme.readHumidity());
-  Serial.println(" %");
-
-  Serial.println();
+    Serial.println();
+    delay(2000);
 }
