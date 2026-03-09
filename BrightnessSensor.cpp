8faab9076d0bc2b34f19fe5840bc99c30fc147c0
illiotes <165102269+4Fillis@users.noreply.github.com>
2026-03-09 14:31:38 +1300

    Add explanatory comments on BrightnessSensor

--- BrightnessSensor.cpp
+++ BrightnessSensor.cpp
@@ -1,13 +1,13 @@
 //TEMT6000 Light Sensor reading
-#define LIGHTSENSORPIN A0 //VP pin
+#define BRIGHTNESSSENSOR A0 //VP pin
 void setup()
 {
-pinMode(LIGHTSENSORPIN, INPUT);
+pinMode(BRIGHTNESSSENSOR, INPUT);
 Serial.begin(115200);
 }
 void loop()
 {
-float reading = analogRead(LIGHTSENSORPIN); //Read light level
+float reading = analogRead(BRIGHTNESSSENSOR); //Read light level
 float square_ratio = reading / 1023.0; //Get percent of maximum value (1023)
 square_ratio = pow(square_ratio, 2.0);
 Serial.println(reading);
