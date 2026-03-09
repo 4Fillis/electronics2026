9dbf24832fdd48a197851fee2b87c6f4d45d28f9
illiotes <165102269+4Fillis@users.noreply.github.com>
2026-03-09 14:35:15 +1300

    Adding indentation to BrightnessSensor loop

--- BrightnessSensor.cpp
+++ BrightnessSensor.cpp
@@ -7,9 +7,25 @@ Serial.begin(115200);
 }
 void loop()
 {
-float reading = analogRead(BRIGHTNESSSENSOR); //Read light level
-float square_ratio = reading / 1023.0; //Get percent of maximum value (1023)
-square_ratio = pow(square_ratio, 2.0);
-Serial.println(reading);
-delay(300);
+    float reading = analogRead(BRIGHTNESSSENSOR); //Read light level
+    float square_ratio = reading / 1023.0; //Get percent of maximum value (1023)
+    square_ratio = pow(square_ratio, 2.0);
+    Serial.println(reading);
+
+    Serial.print(F("Temperature = "));
+    Serial.print(bmp.readTemperature());
+    Serial.println(" *C");
+
+    Serial.print(F("Pressure = "));
+    Serial.print(bmp.readPressure());
+    Serial.println(" Pa");
+
+    Serial.print(F("Approx altitude = "));
+    Serial.print(bmp.readAltitude(1013.25)); /* Adjusted to local forecast! */
+    Serial.println(" m");
+
+    Serial.println();
+
+
+    delay(300);
 }
